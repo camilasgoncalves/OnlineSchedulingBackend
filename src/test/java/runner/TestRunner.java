d33bff4b60aef369beb39fff740928fb1a673673
Camila Gonçalves <c.goncalves@pipastudios.com>
2025-10-21 15:44:04 -0300

    Workflow

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,6 +1,10 @@
 package runner;
 
 import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.*;
 
@@ -8,7 +12,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps,hooks,utils")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/index.html, json:target/cucumber-reports/cucumber.json, junit:target/cucumber-reports/cucumber.xml")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@Test")
 public class TestRunner {
 }
